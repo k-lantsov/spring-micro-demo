ca2821e80067f5338b0cbed281f81b37ffb333b3
Kanstantsin Lantsov <kanstantsin_lantsov@senla.eu>
2022-04-26 11:41:33 +0300

    api-gateway was added

---
+++ api-gateway/src/test/java/com/company/micro/demo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.micro.demo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
